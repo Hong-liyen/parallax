723d01053febfb2c68ae6c13e2e364744a0493b2
Li-Yen Hong <amanda770328@gmail.com>
2017-08-15 13:58:55 +0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+(function($) {
+
+    $(document).ready(function() {
+
+        $('body').scrollspy({
+            target: '.navbar-custom',
+            offset: 50
+        })
+
+        $(document).on('click', '.navbar-collapse.in', function(e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+
+        $('a[href*=#]').bind("click", function(e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top
+            }, 1000);
+            e.preventDefault();
+        });
+
+        /* ---------------------------------------------- /*
+         * Navbar
+        /* ---------------------------------------------- */
+
+        var navbar = $('.navbar');
+        var navHeight = navbar.height();
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() >= navHeight) {
+                navbar.addClass('navbar-color');
+            } else {
+                navbar.removeClass('navbar-color');
+            }
+        });
+
+        if ($(window).width() <= 767) {
+            navbar.addClass('custom-collapse');
+        }
+
+        $(window).resize(function() {
+            if ($(this).width() <= 767) {
+                navbar.addClass('custom-collapse');
+            } else {
+                navbar.removeClass('custom-collapse');
+            }
+        });
+
+        /* ---------------------------------------------- /*
+         * Modal
+        /* ---------------------------------------------- */
+
+        $(".modal-wide").on("show.bs.modal", function() {
+            var height = $(window).height();
+        });
+    });
+
+})(jQuery);
